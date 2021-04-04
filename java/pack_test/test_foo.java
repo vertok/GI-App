6250b533fb7cd93669723c9ab225c00d73b3aad5
vertok <vertok@hotmail.com>
2021-04-04 16:32:39 +0200

    work with java

---
+++ java/pack_test/test_foo.java
@@ -0,0 +1,25 @@
+//package pack_test;
+import java.sql.*;
+public class test{
+    public static void main(String [] args) 
+        {
+            String url = "jdbc:postgresql://kdbs2.dom.htw-dresden.de:5432/gis";
+            String user = "s77673";
+            String pw = "Bjye074#";
+
+            try 
+            {
+                DriverManager.registerDriver(new org.postgresql.Driver());
+            }
+            catch (SQLException e) 
+            {
+                System.out.println("Fehler beim Laden des Treibers");
+            }
+
+            //try {	
+            Connection dbconn = DriverManager.getConnection(url, user, pw);
+            //}
+            //catch (SQLException e) {
+              //  System.out.println("Fehler: Keine Verbindung zur Datenbank!\nÜberprüfen Sie bitte Ihre Anmeldedaten!");
+        }
+        }
